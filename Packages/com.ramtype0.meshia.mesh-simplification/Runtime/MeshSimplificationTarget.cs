c164c0572ffeda50dcf4323f81a28df56e30b989
Ram.Type-0 <p0211@live.jp>
2025-03-25 00:20:15 +0900

    Implement IEquatable for `MeshSimplificationTarget`

--- Packages/com.ramtype0.meshia.mesh-simplification/Runtime/MeshSimplificationTarget.cs
+++ Packages/com.ramtype0.meshia.mesh-simplification/Runtime/MeshSimplificationTarget.cs
@@ -4,11 +4,37 @@ using UnityEngine;
 namespace Meshia.MeshSimplification
 {
     [Serializable]
-    public struct MeshSimplificationTarget
+    public struct MeshSimplificationTarget : IEquatable<MeshSimplificationTarget>
     {
         public MeshSimplificationTargetKind Kind;
         [Min(0)]
         public float Value;
+
+        public override bool Equals(object obj)
+        {
+            return obj is MeshSimplificationTarget target && Equals(target);
+        }
+
+        public bool Equals(MeshSimplificationTarget other)
+        {
+            return Kind == other.Kind &&
+                   Value == other.Value;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Kind, Value);
+        }
+
+        public static bool operator ==(MeshSimplificationTarget left, MeshSimplificationTarget right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(MeshSimplificationTarget left, MeshSimplificationTarget right)
+        {
+            return !(left == right);
+        }
     }
 }
 
