7550ee73512a6c33420e198c3a4d3e9766558075
Ram.Type-0 <p0211@live.jp>
2025-05-13 03:02:11 +0900

    Add Bake mesh button to `NdmfMeshSimplifier`

--- Packages/com.ramtype0.meshia.mesh-simplification/Ndmf/Editor/NdmfMeshSimplifierEditor.cs
+++ Packages/com.ramtype0.meshia.mesh-simplification/Ndmf/Editor/NdmfMeshSimplifierEditor.cs
@@ -1,4 +1,8 @@
+#nullable enable
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using UnityEditor;
+using UnityEngine;
 
 namespace Meshia.MeshSimplification.Ndmf.Editor
 {
@@ -7,10 +11,63 @@ namespace Meshia.MeshSimplification.Ndmf.Editor
     {
         public override void OnInspectorGUI()
         {
+            
+
+
 #if !ENABLE_NDMF
             EditorGUILayout.HelpBox("This component has no effect without NDMF imported to the project.", MessageType.Warning);
 #endif
             base.OnInspectorGUI();
+
+            if (targets.Length == 1)
+            {
+                var ndmfMeshSimplifier = (NdmfMeshSimplifier)target;
+
+                if (TryGetTargetMesh(ndmfMeshSimplifier, out var targetMesh))
+                {
+                    if (GUILayout.Button("Bake mesh"))
+                    {
+                        var absolutePath = EditorUtility.SaveFilePanel(
+                                    title: "Save baked mesh",
+                                    directory: "",
+                                    defaultName: $"{targetMesh.name}-Simplified.asset",
+                                    extension: "asset");
+
+                        if (!string.IsNullOrEmpty(absolutePath))
+                        {
+                            Mesh simplifiedMesh = new();
+
+                            MeshSimplifier.Simplify(targetMesh, ndmfMeshSimplifier.target, ndmfMeshSimplifier.options, simplifiedMesh);
+
+                            AssetDatabase.CreateAsset(simplifiedMesh, Path.Join("Assets/", Path.GetRelativePath(Application.dataPath, absolutePath)));
+                        }
+                    }
+                }
+
+            }
+        }
+
+        private static bool TryGetTargetMesh(NdmfMeshSimplifier ndmfMeshSimplifier, [NotNullWhen(true)] out Mesh? targetMesh)
+        {
+            targetMesh = null;
+            if (ndmfMeshSimplifier.TryGetComponent<MeshFilter>(out var meshFilter))
+            {
+                targetMesh = meshFilter.sharedMesh;
+                if (targetMesh != null) 
+                {
+                    return true;
+                }
+            }
+            if (ndmfMeshSimplifier.TryGetComponent<SkinnedMeshRenderer>(out var skinnedMeshRenderer))
+            {
+                targetMesh = skinnedMeshRenderer.sharedMesh; 
+                if (targetMesh != null)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
