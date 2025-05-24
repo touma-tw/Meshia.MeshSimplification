56eec5dcb574fbfd101f5013a4314f678303d605
Ram.Type-0 <p0211@live.jp>
2025-05-24 11:59:38 +0900

    Add Loop intrinsics

--- Packages/com.ramtype0.meshia.mesh-simplification/Runtime/Jobs/CollectVertexContainingSubMeshIndicesJob.cs
+++ Packages/com.ramtype0.meshia.mesh-simplification/Runtime/Jobs/CollectVertexContainingSubMeshIndicesJob.cs
@@ -1,4 +1,5 @@
 using Unity.Burst;
+using Unity.Burst.CompilerServices;
 using Unity.Collections;
 using Unity.Jobs;
 using UnityEngine;
@@ -15,15 +16,23 @@ namespace Meshia.MeshSimplification
         public void Execute()
         {
             VertexContainingSubMeshIndices.Resize(Mesh.vertexCount, NativeArrayOptions.ClearMemory);
-            for (int i = 0; i < Mesh.subMeshCount; i++)
+            for (int subMeshIndex = 0; subMeshIndex < Mesh.subMeshCount; subMeshIndex++)
             {
-                var subMesh = Mesh.GetSubMesh(i);
-                uint bit = 1u << i;
-                for (int vertexIndex = subMesh.firstVertex, endIndex = subMesh.firstVertex + subMesh.vertexCount; vertexIndex < endIndex; vertexIndex++)
-                {
-                    ref var vertexSubMeshIndex = ref VertexContainingSubMeshIndices.ElementAt(vertexIndex);
-                    vertexSubMeshIndex |= bit;
-                }
+                ProcessSubMesh(subMeshIndex);
+            }
+        }
+
+        private void ProcessSubMesh(int subMeshIndex)
+        {
+            var subMesh = Mesh.GetSubMesh(subMeshIndex);
+            uint bit = 1u << subMeshIndex;
+            for (int vertexIndex = subMesh.firstVertex, endIndex = subMesh.firstVertex + subMesh.vertexCount; vertexIndex < endIndex; vertexIndex++)
+            {
+#if UNITY_BURST_EXPERIMENTAL_LOOP_INTRINSICS
+                Loop.ExpectVectorized();
+#endif
+                ref var vertexSubMeshIndex = ref VertexContainingSubMeshIndices.ElementAt(vertexIndex);
+                vertexSubMeshIndex |= bit;
             }
         }
     }
