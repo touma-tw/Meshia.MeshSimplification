3fef271518b7ccb83a168694911b5bd0861c48ac
tliks <aoft725@gmail.com>
2025-05-25 10:44:15 +0900

    feat: RendererUtility

---
+++ Packages/com.ramtype0.meshia.mesh-simplification/Ndmf/Runtime/RendererUtility.cs
@@ -0,0 +1,40 @@
+#nullable enable
+
+using UnityEngine;
+
+namespace Meshia.MeshSimplification.Ndmf
+{
+    public class RendererUtility
+    {
+        public static Mesh? GetMesh(Renderer renderer)
+        {
+            switch (renderer)
+            {
+                case MeshRenderer meshrenderer:
+                    var meshfilter = meshrenderer.GetComponent<MeshFilter>();
+                    return meshfilter == null ? null : meshfilter.sharedMesh;
+                case SkinnedMeshRenderer skinnedMeshRenderer:
+                    return skinnedMeshRenderer.sharedMesh;
+                default:
+                    return null;
+            }
+        } 
+        
+        public static void AssignMesh(Renderer renderer, Mesh mesh)
+        {
+            switch (renderer)
+            {
+                case MeshRenderer meshrenderer:
+                    var meshfilter = meshrenderer.GetComponent<MeshFilter>();
+                    if (meshfilter == null) return;
+                    meshfilter.sharedMesh = mesh;
+                    break;
+                case SkinnedMeshRenderer skinnedMeshRenderer:
+                    skinnedMeshRenderer.sharedMesh = mesh;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
