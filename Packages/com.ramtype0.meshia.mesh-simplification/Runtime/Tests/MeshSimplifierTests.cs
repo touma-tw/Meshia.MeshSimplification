1565e378ea4d73943568a064f936704a4383d524
Ram.Type-0 <p0211@live.jp>
2025-05-28 00:48:47 +0900

    Add tests

--- Packages/com.ramtype0.meshia.mesh-simplification/Runtime/Tests/MeshSimplifierTests.cs
+++ Packages/com.ramtype0.meshia.mesh-simplification/Runtime/Tests/MeshSimplifierTests.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Meshia.MeshSimplification;
 using NUnit.Framework;
+using Unity.Collections;
+using Unity.Jobs;
 using UnityEngine;
 using UnityEngine.TestTools;
 
@@ -31,9 +33,75 @@ namespace Meshia.MeshSimplification.Tests
             };
             Mesh simplifiedMesh = new();
             await MeshSimplifier.SimplifyAsync(mesh, target, MeshSimplifierOptions.Default, simplifiedMesh);
+            Assert.LessOrEqual(simplifiedMesh.vertexCount, mesh.vertexCount * 0.5f);
             Object.Destroy(simplifiedMesh);
         }
+        [TestCase(PrimitiveType.Sphere)]
+        [TestCase(PrimitiveType.Capsule)]
+        [TestCase(PrimitiveType.Cylinder)]
+        public async Task ShouldSimplifyPrimitiveIncrementally(PrimitiveType type)
+        {
+            var allocator = Allocator.Persistent;
+            var mesh = GetPrimitiveMesh(type);
+            using var blendShapes = BlendShapeData.GetMeshBlendShapes(mesh, allocator);
 
+            Assert.Zero(blendShapes.Length, "Primitive meshes should not have blend shapes by default.");
+            var meshDataArray = Mesh.AcquireReadOnlyMeshData(mesh);
+            var meshData = meshDataArray[0];
+
+            using MeshSimplifier meshSimplifier = new(allocator);
+
+            var load = meshSimplifier.ScheduleLoadMeshData(meshData, MeshSimplifierOptions.Default);
+
+            while (!load.IsCompleted)
+            {
+                await Task.Yield();
+            }
+
+            load.Complete();
+
+            Mesh simplifiedMesh1 = await SimplifyToTarget(new()
+            {
+                Kind = MeshSimplificationTargetKind.RelativeVertexCount,
+                Value = 0.5f,
+            });
+
+            Assert.LessOrEqual(simplifiedMesh1.vertexCount, mesh.vertexCount * 0.5f);
+
+            Object.Destroy(simplifiedMesh1);
+            Mesh simplifiedMesh2 = await SimplifyToTarget(new()
+            {
+                Kind = MeshSimplificationTargetKind.RelativeVertexCount,
+                Value = 0.3f,
+            });
+
+            Assert.LessOrEqual(simplifiedMesh2.vertexCount, mesh.vertexCount * 0.3f);
+            Object.Destroy(simplifiedMesh2);
+
+            async Task<Mesh> SimplifyToTarget(MeshSimplificationTarget target)
+            {
+                var destinationMeshDataArray = Mesh.AllocateWritableMeshData(1);
+                var destinationMeshData = destinationMeshDataArray[0];
+
+                Mesh simplifiedMesh = new();
+                var simplify = meshSimplifier.ScheduleSimplify(meshData, blendShapes, target, new JobHandle());
+
+                using NativeList<BlendShapeData> destinationBlendShapes = new(allocator);
+                var write = meshSimplifier.ScheduleWriteMeshData(meshData, blendShapes, destinationMeshData, destinationBlendShapes, simplify);
+
+                while (!write.IsCompleted)
+                {
+                    await Task.Yield();
+                }
+
+                write.Complete();
+
+                Assert.Zero(destinationBlendShapes.Length, "Primitive meshes should not have blend shapes after simplification.");
+
+                Mesh.ApplyAndDisposeWritableMeshData(destinationMeshDataArray, simplifiedMesh);
+                return simplifiedMesh;
+            }
+        }
         [TestCase(PrimitiveType.Sphere)]
         [TestCase(PrimitiveType.Capsule)]
         [TestCase(PrimitiveType.Cylinder)]
@@ -51,6 +119,7 @@ namespace Meshia.MeshSimplification.Tests
             };
             Mesh simplifiedMesh = new();
             await MeshSimplifier.SimplifyAsync(mesh, target, MeshSimplifierOptions.Default, simplifiedMesh);
+            Assert.LessOrEqual(simplifiedMesh.vertexCount, mesh.vertexCount * 0.5f);
             Object.Destroy(mesh);
             Object.Destroy(simplifiedMesh);
         }
